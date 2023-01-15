bd17654bff878d4039656e17ec9e36237855ca01
Michael Götz <mgoetz1995@gmail.com>
2023-01-15 19:22:43 +0100

    chore: add nuxt whitelabel test project

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div ref="mainWrapper" :style="`--color: ${config?.color}`">
+    <NuxtWelcome />
+    {{ config }}
+    <img :src="config?.logo">
+  </div>
+</template>
+<script lang="ts" setup>
+const { data: config } = await useFetch('/api/config')
+const mainWrapper = ref<HTMLElement>()
+useHead({
+  title: config.value?.title,
+  link: [
+    {rel: 'icon', sizes: '192x192', type: 'image/png', href: config.value?.favicon}
+  ]
+})
+//mainWrapper.style.setProperty('--text-color', config.color)
+</script>
+<style>
+div {
+  background-color: var(--color);
+}
+</style>
---
+++ server/api/config.ts
@@ -0,0 +1,7 @@
+export default defineEventHandler((event) => ({
+    api: 'works', 
+    color: 'hsl(260deg, 100%, 90%)', 
+    logo: '/logo1-192x192.png', 
+    favicon: '/logo1-192x192.png', 
+    title: 'Test Title' 
+}))
